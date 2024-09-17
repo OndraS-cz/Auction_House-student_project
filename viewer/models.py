9ae6d757f57799f2db2875d2b0ad0fc400b9c2c5
Ondra Severa <ondra.severa@gmail.com>
2024-09-17 20:36:38 +0200

    FIX models

--- viewer/models.py
+++ viewer/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db.models import Model, CharField, DateTimeField, ForeignKey
+from django.db.models import Model, CharField, DateTimeField, ForeignKey, ManyToManyField, SET_NULL
 from django.forms import IntegerField
 
 
@@ -20,35 +20,35 @@ class ApartmentType(Model):
     property_type = CharField(max_length=30)
 
 
-class PropertyType(Model):
-    house = ForeignKey(House)
-    ground = ForeignKey(Ground)
-    apartment = ForeignKey(Apartment)
-
-
 class House(Model):
     name = CharField(max_length=50)
     area = IntegerField(null=False)
-    property_type = ForeignKey(HouseType)
+    property_type = ForeignKey(HouseType, null=False, blank=False, on_delete=SET_NULL)
     plot_area = IntegerField(null=False)
     garden_area = IntegerField(null=False)
 
 
 class Ground(Model):
     name = CharField(max_length=50)
-    property_type = ForeignKey(GroundType)
+    property_type = ForeignKey(GroundType, null=False, blank=False, on_delete=SET_NULL)
     property_area = IntegerField(null=False)
 
 
 class Apartment(Model):
     name = CharField(max_length=50)
-    property_type = ForeignKey(ApartmentType)
+    property_type = ForeignKey(ApartmentType, null=False, blank=False, on_delete=SET_NULL)
     area = IntegerField(null=False)
+
+
+class PropertyType(Model):
+    house = ForeignKey(House, null=False, blank=False, on_delete=SET_NULL)
+    ground = ForeignKey(Ground, null=False, blank=False, on_delete=SET_NULL)
+    apartment = ForeignKey(Apartment, null=False, blank=False, on_delete=SET_NULL)
 
 
 class Property(Model):
     property = ManyToManyField(PropertyType)
-    city = ForeignKey(Cities)
+    city = ForeignKey(Cities, null=False, blank=False, on_delete=SET_NULL)
     address = CharField(max_length=50)
     estimate_value = IntegerField(null=False)
     auction_assurance = IntegerField(null=False)
